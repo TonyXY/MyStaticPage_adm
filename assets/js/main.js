415b10ffae153cfd614c838cc896f780fd952ceb
小鱼的理想 <yx8826@163.com>
2015-05-18 14:10:29 +0800

    删除多余的asset

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-/** 
-* Project : StaticPage 
-* Author : TonyXiao 
-* Updated : Fri May 08 2015 11:43:29 
-*/ 
-require.config({baseUrl:"js/",map:{"*":{css:"css.min"}},paths:{modernizr:"../lib/modernizr.custom",jquery:"../lib/jquery-2.1.3.min"},waitSeconds:30,shim:{}}),require(["modernizr"],function(a){Modernizr.rgba?"":window.location="np.html"}),require(["jquery"],function(a){$(function(){})});
